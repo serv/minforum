5c6ba81e37b27078f9e70473f3d20b53e59ed44c
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-08 18:24:08 -0400

    new nested routing structure. fixes multiple forums showing same posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Minforum::Application.routes.draw do
   root :to => "forums#index"
   resources :forums do
-    resources :topics
-  end
-  resources :topics do
-    resources :posts
+    resources :topics do 
+      resources :posts
+    end
   end
 end
