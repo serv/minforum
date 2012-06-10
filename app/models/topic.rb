d9e5f0362414bd48fe27595504694ec77e6ef08a
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-10 01:30:46 -0400

    accessor for last_post_id on topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ActiveRecord::Base
   has_many :posts
   belongs_to :forum
   accepts_nested_attributes_for :posts
-  attr_accessible :name, :posts_attributes
+  attr_accessible :name, :last_post_id, :posts_attributes
 end
