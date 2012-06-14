7661f05acb9fdfff95fdf84854f2d787c0f53a73
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-13 23:10:47 -0400

    edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,22 @@ class PostsController < ApplicationController
     end
   end
   
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      topic = Topic.find(@post.topic_id)
+      flash[:success] = "Success!"
+      redirect_to topic_posts_path(topic)
+    else
+      render 'edit'
+    end
+  end
+  
   def destroy
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
