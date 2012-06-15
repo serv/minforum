a5e9650571f0e9d534f89198a316f1fc86814323
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-14 21:32:20 -0400

    forum_helper methods for posts and views

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,2 +1,17 @@
 module ForumsHelper
+  def total_views(forum)
+    views =  0
+    forum.topics.each do |topic|
+      views += topic.views
+    end
+    views
+  end
+  
+  def total_posts(forum)
+    posts_count = 0
+    forum.topics.all.each do |topic|
+      posts_count += topic.posts.count
+    end
+    posts_count -= forum.topics.count
+  end
 end
