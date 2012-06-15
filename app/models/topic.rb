e737ea2b899ad4b6c0781db24bc6d1d6700a6b6a
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-14 21:51:27 -0400

    views on forum index

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,6 @@ class Topic < ActiveRecord::Base
   belongs_to :forum
   accepts_nested_attributes_for :posts, :allow_destroy => true
   attr_accessible :name, :last_post_id, :posts_attributes
-  
   before_save :init
   
   def init
