14a5e5bff59a9471b606537fc4e705ae40dd5e1e
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-06-29 12:06:11 -0400

    replace user placeholder with real user

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def post_creator(post)
+    User.find_by_id(post.user_id)
+  end
+  
+  def post_creator_name(post)
+    post_creator(post).name
+  end
 end
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,22 @@
 module TopicsHelper
+  def topic_creator(topic)
+    User.find_by_id(topic.user_id)
+  end
+  
+  def topic_creator_name(topic)
+    topic_creator(topic).name
+  end
+  
+  def last_post(topic)
+    Post.find_by_id(topic.last_post_id)
+  end
+  
+  def last_post_creator(topic)
+    User.find_by_id(last_post(topic).user_id)
+  end
+  
+  def last_post_creator_name(topic)
+    last_post_creator(topic).name
+  end
+  
 end
