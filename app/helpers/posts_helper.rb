b9461bb635cd8af77f9b51d2faebe794025c184c
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-07-07 12:31:24 -0400

    topic name for posts in user profile page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,14 @@ module PostsHelper
     User.find_by_id(post.user_id)
   end
   
+  def topic_name(post)
+    topic_for_post(post).name
+  end
+  
+  def topic_for_post(post)
+    Topic.find_by_id(post.topic_id)
+  end
+  
   def post_creator_name(post)
     post_creator(post).name
   end
