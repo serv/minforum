c1b96c163b19ac0f0e2482ce7f2ec0b21f3aae48
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2012-07-10 15:35:13 -0400

    user index viewable only to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :setting, :change_password]
+  before_filter :signed_in_user, only: [:edit, :update, :destroy, :setting, :change_password]
   before_filter :correct_user,   only: [:edit, :update, :change_password]
-  before_filter :admin_user,     only: :destroy
+  before_filter :admin_user,     only: [:index, :destroy]
   
   def show
     @user = User.find(params[:id])
